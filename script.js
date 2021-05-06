6bca9f0ad51a9063514a40fd80622f2d30693c9d
Volodymyr <volodymyr.steblii@gmail.com>
2021-05-06 09:41:11 +0300

    Add modules support

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import fetchFilms from './data.js';
+
 let table = document.querySelector('.table');
 const thead = document.createElement('thead');
 const tbody = document.createElement('tbody');
@@ -7,57 +9,7 @@ table.appendChild(thead);
 thead.appendChild(trHead);
 table.appendChild(tbody);
 
-
-const films = [
-    {
-        number: '1',
-        films: 'Papillon',
-        genre: 'Drama, crime',
-        releaseDate: 'September 9, 2017',
-        countries: 'USA',
-        assessment: '8.9'
-    },
-    {
-        number: '2',
-        films: 'The Hangover',
-        genre: 'Сomedy',
-        releaseDate: 'May 30, 2009 ',
-        countries: 'USA',
-        assessment: '9.0'
-    },
-    {
-        number: '3',
-        films: 'Babysitting',
-        genre: 'Сomedy',
-        releaseDate: '16 April 2014',
-        countries: 'France',
-        assessment: '8.7'
-    },
-    {
-        number: '4',
-        films: 'Venom',
-        genre: 'Drama, crime',
-        releaseDate: 'October 1, 2018',
-        countries: 'USA',
-        assessment: '7.8'
-    },
-    {
-        number: '5',
-        films: 'Papillon',
-        genre: 'Superhero movie, crime',
-        releaseDate: 'September 9, 2017',
-        countries: 'USA',
-        assessment: '8.5'
-    },
-    {
-        number: '6',
-        films: 'Papillon',
-        genre: 'Drama, crime',
-        releaseDate: 'September 9, 2017',
-        countries: 'USA',
-        assessment: '8.5'
-    },
-];
+const films = fetchFilms();
 
 //для th
 const title = [{number: 'Number'}, {films: 'Films'}, {genre: 'Genre'}, {releaseDate: 'Release date'}, {countries:'Countries'}, {assessment:'Assessment'}];
