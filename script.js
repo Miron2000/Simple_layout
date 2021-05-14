d1acef47003934aa4d5b94b1b24e27c6ea2d347c
miron <miron.ivasch@gmail.com>
2021-05-14 12:01:16 +0300

    Refactoring code

--- script.js
+++ script.js
@@ -13,243 +13,308 @@ thead.appendChild(trHead);
 table.appendChild(tbody);
 
 class Film {
-  constructor(id, name, genre, releaseDate, countries, assessment) {
-    this.id = id;
-    this.name = name;
-    this.genre = genre;
-    this.releaseDate = releaseDate;
-    this.countries = countries;
-    this.assessment = assessment;
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.countries = countries;
+        this.assessment = assessment;
+    }
 
-  set id(id) {
-    this._id = id;
-  }
+    set id(id) {
+        this._id = id;
+    }
 
-  get id() {
-    return this._id;
-  }
+    get id() {
+        return this._id;
+    }
 
-  set name(name) {
-    this._name = name.trim();
-  }
+    set name(name) {
+        this._name = name.trim();
+    }
 
-  get name() {
-    return this._name;
-  }
+    get name() {
+        return this._name;
+    }
 
-  set genre(genre) {
-    this._genre = genre.trim();
-  }
+    set genre(genre) {
+        this._genre = genre.trim();
+    }
 
-  get genre() {
-    return this._genre;
-  }
+    get genre() {
+        return this._genre;
+    }
 
-  set releaseDate(releaseDate) {
-    this._releaseDate = releaseDate.trim();
-  }
+    set releaseDate(releaseDate) {
+        this._releaseDate = releaseDate.trim();
+    }
 
-  get releaseDate() {
-    return this._releaseDate;
-  }
+    get releaseDate() {
+        return this._releaseDate;
+    }
 
-  set countries(countries) {
-    this._countries = countries.trim();
-  }
+    set countries(countries) {
+        this._countries = countries.trim();
+    }
 
-  get countries() {
-    return this._countries;
-  }
+    get countries() {
+        return this._countries;
+    }
 
-  set assessment(assessment) {
-    this._assessment = assessment;
-  }
+    set assessment(assessment) {
+        this._assessment = assessment;
+    }
 
-  get assessment() {
-    return this._assessment;
-  }
+    get assessment() {
+        return this._assessment;
+    }
 }
 
 class IMDBFilm extends Film {
-  constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
-    super(id, name, genre, releaseDate, countries, assessment);
-    this.imdbFilm = imdbFilm;
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
+        super(id, name, genre, releaseDate, countries, assessment);
+        this.imdbFilm = imdbFilm;
+    }
 
-  set imdbFilm(imdbFilm) {
-    this._imdbFilm = imdbFilm;
-  }
+    set imdbFilm(imdbFilm) {
+        this._imdbFilm = imdbFilm;
+    }
 
-  get imdbFilm() {
-    return this._imdbFilm;
-  }
+    get imdbFilm() {
+        return this._imdbFilm;
+    }
 }
 
 const GENRE = {
-  comedy: "comedy",
-  horror: "horror",
-  drama: "drama",
-  superhero: "superhero",
-  fiction: "fiction",
+    comedy: "comedy",
+    horror: "horror",
+    drama: "drama",
+    superhero: "superhero",
+    fiction: "fiction",
 };
 
 class Horror extends IMDBFilm {
-  constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
-    super(id, name, GENRE.horror, releaseDate, countries, assessment, imdbFilm);
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
+        super(id, name, GENRE.horror, releaseDate, countries, assessment, imdbFilm);
+    }
 }
 
 class Comedy extends IMDBFilm {
-  constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
-    super(id, name, GENRE.comedy, releaseDate, countries, assessment, imdbFilm);
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
+        super(id, name, GENRE.comedy, releaseDate, countries, assessment, imdbFilm);
+    }
 }
 
 class Drama extends IMDBFilm {
-  constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
-    super(id, name, GENRE.drama, releaseDate, countries, assessment, imdbFilm);
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
+        super(id, name, GENRE.drama, releaseDate, countries, assessment, imdbFilm);
+    }
 }
 
 class SuperheroMovie extends IMDBFilm {
-  constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
-    super(
-      id,
-      name,
-      GENRE.superhero,
-      releaseDate,
-      countries,
-      assessment,
-      imdbFilm
-    );
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
+        super(
+            id,
+            name,
+            GENRE.superhero,
+            releaseDate,
+            countries,
+            assessment,
+            imdbFilm
+        );
+    }
 }
 
 class Fiction extends IMDBFilm {
-  constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
-    super(
-      id,
-      name,
-      GENRE.fiction,
-      releaseDate,
-      countries,
-      assessment,
-      imdbFilm
-    );
-  }
+    constructor(id, name, genre, releaseDate, countries, assessment, imdbFilm) {
+        super(
+            id,
+            name,
+            GENRE.fiction,
+            releaseDate,
+            countries,
+            assessment,
+            imdbFilm
+        );
+    }
 }
 
 const films = fetchFilms();
 films.forEach((item) => {
-  function detailsConstructor(constructorName) {
-    new constructorName(
-      item.number,
-      item.films,
-      item.genre,
-      item.releaseDate,
-      item.countries,
-      item.assessment,
-      item.imdbFilm
-    );
-  }
+    function detailsConstructor(constructorName) {
+        new constructorName(
+            item.number,
+            item.films,
+            item.genre,
+            item.releaseDate,
+            item.countries,
+            item.assessment,
+            item.imdbFilm
+        );
+    }
 
-  switch (item.genre.split(", ")[0].toLowerCase()) {
-    case GENRE.comedy:
-      detailsConstructor(Comedy);
-      break;
+    switch (item.genre.split(", ")[0].toLowerCase()) {
+        case GENRE.comedy:
+            detailsConstructor(Comedy);
+            break;
 
-    case GENRE.horror:
-      detailsConstructor(Horror);
-      break;
+        case GENRE.horror:
+            detailsConstructor(Horror);
+            break;
 
-    case GENRE.drama:
-      detailsConstructor(Drama);
-      break;
+        case GENRE.drama:
+            detailsConstructor(Drama);
+            break;
 
-    case GENRE.superhero:
-      detailsConstructor(SuperheroMovie);
-      break;
+        case GENRE.superhero:
+            detailsConstructor(SuperheroMovie);
+            break;
 
-    case GENRE.fiction:
-      detailsConstructor(Fiction);
-      break;
-  }
+        case GENRE.fiction:
+            detailsConstructor(Fiction);
+            break;
+    }
 
-  detailsConstructor(IMDBFilm);
+    detailsConstructor(IMDBFilm);
 });
 
 const column = [
-  { acessor: "number", title: "Number", data: "integer" },
-  { acessor: "name", title: "Films", data: "text" },
-  { acessor: "genre", title: "Genre", data: "text" },
-  { acessor: "releaseDate", title: "Release date", data: "date" },
-  { acessor: "countries", title: "Countries", data: "text" },
-  { acessor: "assessment", title: "Rating", data: "double" },
-  { acessor: "imdbFilm", title: "IMDB", data: "boolean" },
+    {acessor: "number", title: "Number", data: "integer"},
+    {acessor: "name", title: "Films", data: "text"},
+    {acessor: "genre", title: "Genre", data: "text"},
+    {acessor: "releaseDate", title: "Release date", data: "date"},
+    {acessor: "countries", title: "Countries", data: "text"},
+    {acessor: "assessment", title: "Rating", data: "double"},
+    {acessor: "imdbFilm", title: "IMDB", data: "boolean"},
 ];
 
 //for th
 function createTableTitles(trHead) {
-  column.forEach((item) => {
-    const th = document.createElement("th");
-    th.innerHTML = item.title;
-    trHead.appendChild(th);
-    th.setAttribute("data-type", item.data);
-  });
+    column.forEach((item) => {
+        const th = document.createElement("th");
+        th.innerHTML = item.title;
+        trHead.appendChild(th);
+        th.setAttribute("data-type", item.data);
+    });
 }
 
 //for td
 function createTableRows(tbody) {
-  for (let item of films) {
-    const tr = document.createElement("tr");
+    for (let item of films) {
+        const tr = document.createElement("tr");
 
-    function createTableColumnFilms(itemText) {
-      let td = document.createElement("td");
-      td.innerHTML = itemText;
-      tr.appendChild(td);
+        function createTableColumnFilms(itemText) {
+            let td = document.createElement("td");
+            td.innerHTML = itemText;
+            tr.appendChild(td);
+        }
+
+        column.forEach((i) => {
+            switch (i.acessor) {
+                case "number":
+                    createTableColumnFilms(item.number);
+                    break;
+
+                case "name":
+                    createTableColumnFilms(item.films);
+                    break;
+
+                case "genre":
+                    createTableColumnFilms(item.genre);
+                    break;
+
+                case "releaseDate":
+                    createTableColumnFilms(item.releaseDate);
+                    break;
+
+                case "countries":
+                    createTableColumnFilms(item.countries);
+                    break;
+
+                case "assessment":
+                    createTableColumnFilms(item.assessment);
+                    break;
+
+                case "imdbFilm":
+                    createTableColumnFilms(item.imdbFilm);
+                    break;
+            }
+        });
+
+        tbody.appendChild(tr);
     }
-
-    column.forEach((i) => {
-      switch (i.acessor) {
-        case "number":
-          createTableColumnFilms(item.number);
-          break;
-
-        case "name":
-          createTableColumnFilms(item.films);
-          break;
-
-        case "genre":
-          createTableColumnFilms(item.genre);
-          break;
-
-        case "releaseDate":
-          createTableColumnFilms(item.releaseDate);
-          break;
-
-        case "countries":
-          createTableColumnFilms(item.countries);
-          break;
-
-        case "assessment":
-          createTableColumnFilms(item.assessment);
-          break;
-
-        case "imdbFilm":
-          createTableColumnFilms(item.imdbFilm);
-          break;
-      }
-    });
-
-    tbody.appendChild(tr);
-  }
 }
 
 createTableTitles(trHead);
 createTableRows(tbody);
 
-///////////////////////////ПРОСТАЯ СОРТИРОВКА..........................................
+///////////////////////////QUICK SORT/////////////////////////////////////////
+let activeColumnIndex = -1;
+
+table.addEventListener("click", (event) => {
+    let elTarget = event.target;
+    const index = elTarget.cellIndex; //index елемента(th) на который кликнули
+    const type = elTarget.getAttribute("data-type");
+
+    if (elTarget.tagName !== "TH") return;
+
+    sortTable(index, type, activeColumnIndex === index);
+    activeColumnIndex = activeColumnIndex === index ? -1 : index;
+});
+
+function sortTable(index, type, isSorted) {
+    let rowsArr = [].slice.call(tbody.rows);
+    const sortArr = quickSort(rowsArr, index);
+
+    if (isSorted) {
+        sortArr.reverse();
+    }
+
+    table.removeChild(tbody);
+
+    for (let i = 0; i < sortArr.length; i++) {
+        tbody.appendChild(sortArr[i]);
+    }
+
+    table.appendChild(tbody);
+
+    function getColumnValue(row, cellIndex) {
+        if (type === "integer" || type === "double") {
+            return +row.cells[cellIndex].innerText;
+        } else if (type === "text") {
+            return row.cells[cellIndex].innerText;
+        } else if (type === "date") {
+            const dateA = row.cells[cellIndex].innerText.split(".").reverse().join("-");
+            return new Date(dateA).getTime();
+        } else {
+            throw new Error("Invalid type. Please choose another type");
+        }
+    }
+
+    function quickSort(arr, columnIndex) {
+        if (arr.length < 2) return arr;
+        let pivotRow = arr[0];
+        let pivot = getColumnValue(pivotRow, columnIndex);
+        const left = [];
+        const right = [];
+
+        for (let i = 1; i < arr.length; i++) {
+            const columnValue = getColumnValue(arr[i], columnIndex);
+            if (pivot > columnValue) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
+            }
+        }
+      return [...quickSort(left, columnIndex), pivotRow, ...quickSort(right, columnIndex)];
+    }
+}
+
+
+
+
+///////////////////////////ПРОСТАЯ СОРТИРОВКА/////////////////////////////////////
 // let activeColumnIndex = -1;
 // const sortTable = function(tbody, index, type, isSorted) {
 //
@@ -301,86 +366,3 @@ createTableRows(tbody);
 //     sortTable(tbody, index, type, activeColumnIndex === index);
 //     activeColumnIndex = (activeColumnIndex === index) ? -1 : index;
 // })
-
-///////////////////////////БЫСТРАЯ СОРТИРОВКА..........................................
-let activeColumnIndex = -1;
-table.addEventListener("click", (event) => {
-  let elTarget = event.target;
-  const index = elTarget.cellIndex; //index елемента(th) на который кликнули
-  const type = elTarget.getAttribute("data-type");
-
-  if (elTarget.tagName !== "TH") return;
-
-  sortTable(index, type, activeColumnIndex === index);
-  activeColumnIndex = activeColumnIndex === index ? -1 : index;
-});
-
-function sortTable(index, type, isSorted) {
-  let rowsArr = [].slice.call(tbody.rows);
-
-  const sortArr = quickSort(rowsArr, index);
-
-  if (isSorted) {
-    sortArr.reverse();
-  }
-  table.removeChild(tbody);
-
-  for (let i = 0; i < sortArr.length; i++) {
-    tbody.appendChild(sortArr[i]);
-  }
-
-  table.appendChild(tbody);
-
-  function getColumnValue(row, cellIndex) {
-    if (type === "integer" || type === "double") {
-      return +row.cells[cellIndex].innerText;
-    } else if (type === "text") {
-      return row.cells[cellIndex].innerText;
-    } else if (type === "date") {
-      const dateA = row.cells[cellIndex].innerText
-        .split(".")
-        .reverse()
-        .join("-");
-      return new Date(dateA).getTime();
-    } else {
-      throw new Error("Invalid type");
-    }
-  }
-
-  function quickSort(arr, columnIndex) {
-    if (arr.length < 2) return arr;
-    let pivorRow = arr[0];
-    let pivot = getColumnValue(pivorRow, columnIndex);
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-      const columnValue = getColumnValue(arr[i], columnIndex);
-      if (pivot > columnValue) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
-    }
-    return quickSort(left, columnIndex).concat(pivorRow, quickSort(right, columnIndex));
-  }
-
-  /*
-    function quickSort(rowsArr) {
-
-
-
-        if (rowsArr.length <= 1) {
-            return rowsArr;
-        } else {
-            const pivot = rowsArr[Math.floor(Math.random() * rowsArr.length)];//рандомный обьект с фильмом <tr></tr>
-            const pivotValues = getColumnValue(pivot, index);//дает строку или число с обьекта, смотря на то что нажму
-
-            const less = rowsArr.filter(row => getColumnValue(row, index) < pivotValues);//массив с маленькими числами
-            const greater = rowsArr.filter(row => getColumnValue(row, index) > pivotValues);//массив с большими числами
-
-            return [...quickSort(less), pivot, ...quickSort(greater)];
-        }
-    }
-    */
-}
